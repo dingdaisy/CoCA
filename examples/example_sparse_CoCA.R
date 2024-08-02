73b93d781765c221a6e28df488c74fa543eeeaa1
Daisy Ding <dingd@stanford.edu>
2024-08-02 14:06:57 -0700

    add CV + example script for sparse coca

---
+++ examples/example_sparse_CoCA.R
@@ -0,0 +1,57 @@
+# Example script for running sparse CoCA on a dataset generated from a latent factor model
+
+# Source the COCA functions
+source("./../functions/sparse_CoCA.R")
+source("./../functions/CoCA.R")
+source("./../functions/utils.R")
+
+# Load the simulated data
+
+############################################################################
+# simData1 is a pre-generated dataset for testing the CoCA framework, 
+# generated from the latent factor model:
+#
+# x = (u,v) ~ (beta_u,beta_v) z  + Eta_u %*% zu  + Eta_v %*% zv  +  w, 
+# 
+# where
+# 
+# z ~ N(0,1), zu,zv ~ N(0,I_k) w ~ N(0,sigma^2 I_p), all independent.
+# 
+# and supp(cols(Eta_u)) \subset 1:pu, supp(cols(Eta_v)) \subset (pu + 1):p.
+############################################################################
+
+load("./../data/simData2.RData")
+X <- simData2$X # The combined data matrix
+i <- simData2$i # Indices partitioning the variables into two views
+
+# Fit sparse CoCA model with rho >= 0
+rho <- 1e-3 # Weight on the agreement penalty
+lambda <- 1e-3 # Weight on the sparsity penalty
+result = sparse_coca(X, i, rho, lambda, maxiter = 100, eps = 1e-8)
+
+# Extract the results
+v = result$v  # Coefficients of the identified component
+
+# Evaluate the estimation error
+estimation_error = evaluate_mse(normalize_vector(v), normalize_vector(simData2$beta))
+print(sprintf("The estimation error with rho = %.3f is %.3f", rho, estimation_error))
+
+# Cross-validation for CoCA
+nfolds = 3
+nlambdas = 30 
+nrhos = 30 
+rhos = c(0, exp(seq(log(1e-5),log(10),length.out = nrhos)))
+cv_results = sparse_coca_cv(X, rhos, nlambdas, nfolds, simData2$pu, simData2$pv)
+
+# Plot CV curve
+par(mfrow = c(1, 1))
+plot_cv_coca(cv_results)
+
+# Apply sparse CoCA with selected best parameters
+result_selected = sparse_coca(X, i, cv_results$best_rho, cv_results$best_lambda)
+v_selected = result_selected$v  
+estimation_error_selected = evaluate_mse(normalize_vector(v_selected), normalize_vector(simData2$beta))
+print(sprintf("The estimation error with rho = %.3f is %.3f", cv_results$best_rho, estimation_error_selected))
+
+
+
--- functions/sparse_CoCA.R
+++ functions/sparse_CoCA.R
@@ -1,4 +1,7 @@
 
+library(Matrix)
+library(glmnet)
+
 # Computes the solution to the optimization problem in the SPARSE CoCA framework:
 # 
 # min ||X - u v^{T}||_F^2 + rho D(v) + lambda |v|_1 s.t. ||u||^2 <= 1
@@ -20,7 +23,6 @@
 #
 # Returns:
 #   A list containing:
-# A list containing:
 # - v: A numeric vector representing the learned coefficients of the component, of dimension p.
 # - u: A numeric vector corresponding to the learned scores of the component, of dimension n.
 
@@ -66,3 +68,154 @@ sparse_coca = function(X,i,rho,lambda = 0,start = NULL,eps = 1e-8,maxiter = 100,
 
   return(list(v = v, u = u))
 }
+
+# Perform cross-validation for CoCA to select the best parameters.
+# The data matrix X is split into nfolds, and for each fold, the model 
+# is trained on the training set and evaluated on the validation set for reconstruction error.. 
+# The mean squared error is computed for each combination 
+# of rho and lambda, and the results are averaged across folds.
+#
+# Note: There are different options for CV procedures to determine the optimal values 
+# of hyperparameters rho and lambda in CoCA, including K-fold CV and "speckled CV" for 
+# unsupervised settings, and K-fold CV for supervised settings. 
+# We implement here the K-fold CV.
+#
+# Args:
+#   X: A combined data matrix.
+#   rhos: A numeric vector of rho values (weights on the agreement penalty).
+#   n_lambdas: An integer representing the number of lambda values to select from.
+#   nfolds: An integer representing the number of folds for cross-validation.
+#   pu: An integer representing the dimension of the first data view.
+#   pv: An integer representing the dimension of the second data view.
+#   eps: A numeric value representing the convergence criterion.
+#
+# Returns:
+#   A list containing:
+#     results: An array of cross-validation errors for each combination of rho and lambda across all folds.
+#     mean_errors: A matrix of mean cross-validation errors for each combination of rho and lambda.
+#     median_errors: A matrix of median cross-validation errors for each combination of rho and lambda.
+#     lambdas: A matrix of lambda sequences for each rho value.
+#     best_rho: The rho value with the minimum mean error.
+#     best_lambda: The lambda value with the minimum mean error.
+#     best_rho_median: The rho value with the minimum median error.
+#     best_lambda_median: The lambda value with the minimum median error.
+
+sparse_coca_cv <- function(X, rhos, n_lambdas, nfolds, pu, pv, eps = 1e-8, maxiter = 100) {
+  
+  # Create folds for cross-validation
+  folds <- cut(seq(1, nrow(X)), breaks = nfolds, labels = FALSE)
+  lambdas <- matrix(nrow = length(rhos), ncol = n_lambdas)
+  results <- array(dim = c(length(rhos), n_lambdas, nfolds))
+  p = pu + pv
+  n = nrow(X)
+  
+  # Loop over each rho value
+  for (ii in 1:length(rhos)) {
+    fits = list()
+    
+    # Loop over each fold
+    for (k in 1:nfolds) {
+      
+      # Split data into training and validation
+      train_idx <- which(folds != k)
+      valid_idx <- which(folds == k)
+      X_train <- X[train_idx, ]
+      X_valid <- X[valid_idx, ]
+      
+      # Choose lambda sequence during the first fold
+      if (k == 1){
+        # Choose lambda as would be done in first step of alternating algorithm,
+        # initialized at the non-sparse solution to CoCA.
+        # Here we use X for choosing lambda.
+        # Only do this once for the first fold split and use the same sequence for the rest.
+        nosparse = fit_coca(X,i = 1:pu,rho = rhos[ii],debug = F,eps = eps, maxiter = maxiter)
+        u = nosparse$u
+        d = rep(1,p); d[-(1:pu)] = -1; D = diag(d)
+        Xv = rbind(Diagonal(p), sqrt(rhos[ii])* X %*% D)
+        yv = c(t(X) %*% u,rep(0,n))
+        
+        # Fit glmnet to obtain the lambda sequence.
+        lambdaseq = glmnet::glmnet(x = Xv, y = yv, nlambda = n_lambdas - 1,intercept = F,standardize = F)$lambda
+        
+        # Pad the lambda sequence with 0s to make it the desired length.
+        if (length(lambdaseq) < n_lambdas) lambdaseq = c(lambdaseq,rep(0,n_lambdas - length(lambdaseq)))
+        lambdas[ii,] = lambdaseq / 20
+      }
+      
+      # Fit non-sparse CoCA on the training data
+      nosparse_fold = fit_coca(X_train,i = 1:pu,rho = rhos[ii],debug = F,eps = eps, maxiter = maxiter)
+      
+      # Loop over each lambda value
+      for (jj in 1:n_lambdas) {
+        # Warm start the fit with the previous solution
+        start <- if (jj == 1) nosparse_fold$u else fits[[jj - 1]]$u
+        
+        # Fit sparse CoCA on the training data
+        fits[[jj]] <- sparse_coca(X_train, i = 1:pu, rho = rhos[ii], lambda = lambdas[ii, jj], start = start, eps = eps, maxiter = maxiter, debug = F)
+        
+        # Evaluate on validation data for reconstruction error
+        fold_errors <- mean((proj_vector(X_valid, normalize_vector(fits[[jj]]$v)) - X_valid)^2) 
+        results[ii, jj, k] <- fold_errors
+      }
+    }
+  }
+  
+  # Calculate mean errors across all folds
+  mean_errors <- apply(results, c(1, 2), mean)
+  median_errors <- apply(results, c(1, 2), median)
+  
+  # Find the best rho and lambda based on the minimum mean/median error
+  min_error_idx <- which(mean_errors == min(mean_errors, na.rm=TRUE), arr.ind = TRUE)
+  best_rho_mean <- rhos[min_error_idx[1]]
+  best_lambda_mean <- lambdas[min_error_idx[2]]
+  
+  min_error_idx_median <- which(median_errors == min(median_errors, na.rm=TRUE), arr.ind = TRUE)
+  best_rho_median <- rhos[min_error_idx_median[1]]
+  best_lambda_median <- lambdas[min_error_idx_median[2]]
+  
+  # Return the results
+  list(results = results,
+       mean_errors = mean_errors, 
+       median_errors = median_errors,
+       lambdas = lambdas,
+       rhos = rhos,
+       best_rho = best_rho_mean,
+       best_lambda = best_lambda_mean,
+       best_rho_median = best_rho_median,
+       best_lambda_median = best_lambda_median)
+}
+
+# Plots the cross-validation errors as a function of rho.
+#
+# Args:
+#   coca_results: A list containing the results of sparse_coca_cv.
+#   by: A character string indicating whether to summarize by 'mean_error' or 'median_error'.
+#
+# Returns:
+#   A plot of the cross-validation errors.
+
+plot_cv_coca <- function(coca_results, by = "mean_error") {
+  if (by == "mean_error") {
+    ave_loss_by_rho = apply(coca_results$mean_errors,1,min)
+    plot(log(coca_results$rhos), log(ave_loss_by_rho), 
+         main = "CV Reconstruction Error\n(Summarized by Mean)", 
+         xlab = expression(log(rho)), 
+         ylab = "log(Reconstruction error)",
+         pch = 19, # Solid circle
+         cex = 0.7,  # Size of points
+         col = "black")
+    
+  } else if (by == "median_error") {
+    ave_loss_by_rho = apply(coca_results$median_errors,1,min)
+    plot(log(coca_results$rhos), log(ave_loss_by_rho), 
+         main = "CV Reconstruction Error\n(Summarized by Median)", 
+         xlab = expression(log(rho)), 
+         ylab = "log(Reconstruction error)",
+         pch = 19, # Solid circle
+         cex = 0.7,  # Size of points
+         col = "black")
+  } else {
+    stop("Invalid 'by' argument. Please specify 'mean_error' or 'median_error'.")
+  }
+}
+
